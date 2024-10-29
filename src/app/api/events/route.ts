4ef80ff16c364db3de1ea70278a6bbbfb149453b
Harshit Sharma <cwd.harshit911@gmail.com>
2024-10-29 13:30:24 +0530

    Refactor: Update event fetching to order by date in descending order

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const events = await prisma.event.findMany();
+    const events = await prisma.event.findMany({
+      orderBy: {
+        date: "desc",
+      },
+    });
     return NextResponse.json(
       {
         success: true,
