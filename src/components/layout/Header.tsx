b841a19cae45678376c1686d3c8c10b17c299000
Harshit Sharma <cwd.harshit911@gmail.com>
2024-10-29 08:54:58 +0530

    Refactor: Remove commented console.log statement in Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
     return () => clearInterval(timer);
   }, []);
 
-  //   console.log(user);
+  //    console.log(user);
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
