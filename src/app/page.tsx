a7950f72c21327ba182bc2da4111c9061f63c2d8
DhruvVerseX <dhruvnegi2040@gmail.com>
2024-10-30 15:40:04 +0530

    refactor: Update page component and dependencies
    Refactor the page component to import and render the DashboardHomeComponent from the '@/components/dashboard-home' module. Also update the package.json file to add new dependencies: @react-three/drei, @react-three/fiber, and recharts.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>s</div>;
+import { DashboardHomeComponent } from '@/components/dashboard-home'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <DashboardHomeComponent />
+    </div>
+  )
 }
+
+export default page
